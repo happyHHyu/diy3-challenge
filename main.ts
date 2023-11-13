cadea81c403b4153f25f4327942c6622ca67ef51
happyHHyu <117804376+happyHHyu@users.noreply.github.com>
2023-11-13 10:09:53 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    ans = Quantity / 2 * Unit_price
+    basic.showNumber(ans)
+})
+let ans = 0
+let Quantity = 0
+let Unit_price = 0
+Unit_price = 8
+Quantity = 10
+basic.forever(function () {
+	
+})
